a7058027ab8c876d4e099f52993534b4d6bd434a
gaowanlu <2209120827@qq.com>
2021-03-19 17:54:42 +0800

    添加修改头像功能，实现头像替换与加载浮层的联系

--- JSView/js/UserPersonal/UserData/userDataPage.js
+++ JSView/js/UserPersonal/UserData/userDataPage.js
@@ -23,7 +23,7 @@
     '       </div>',
     '       <div style="height:80px;width:100%;"></div>',
     '        <!-- 头像栏 -->',
-    '        <div class="hoverPointer" style="width:640px;height:130px;background-color:#ffffff ;display: flex;">',
+    '        <div class="hoverPointer" onclick="SlagoModel.UserPersonal.UserData.userDataPage.HeadImgSeting(this)" style="width:640px;height:130px;background-color:#ffffff ;display: flex;">',
     '            <!-- 字体提示栏 -->',
     '            <div style="width:150px;height:130px;background-color: rgb(255, 255, 255);',
     '            display: flex;align-items: center;font-size: 25px;margin-left: 30px;">',
@@ -38,8 +38,9 @@
     '            <div style="height: 100%;width:40px;background-color: rgb(255, 255, 255);display: flex;justify-content: center;align-items: center;">',
     '                <img src="./img/箭头_右.png" style="width:40px;">',
     '            </div>',
-    '',
+    '            <input type="file" accept="image/*" style="display: none;">',
     '        </div>',
+
     '        <div style="width:610px;height:1px;background-color: #f0f0f0;margin-left: 30px;"></div>',
     '        <!-- 昵称栏 -->',
     '        <div onclick="SlagoModel.UserPersonal.UserData.nameSetingPage.show()" class="hoverPointer" style="width:640px;height:100px;background-color: #ffffff;display: flex;">',
@@ -118,13 +119,38 @@
     '        </div>',
     '    </div>',
     ].join("");
+
+    //头像设置
+    namespace.HeadImgSeting=function(dom){
+        let input=dom.children[dom.children.length-1];
+        //input添加状态改变事件
+        input.onchange=function(){
+            //获得input下面的头像img节点
+            let imgNode=this.parentNode.children[1].children[0];
+            //检测图像文件是否选择
+            if(this.files!=undefined&&this.files.length>0&&this.files&&this.files[0]){
+                if(this.files[0].getAsDataURL){
+                    imgNode.src=this.files[0].getAsDataURL;
+                    Slago.LoadPage.hover();//进行悬浮层
+                }else{
+                    imgNode.src=window.URL.createObjectURL(this.files[0]);
+                    Slago.LoadPage.hover();//进行悬浮层
+                }
+            }else if(input_file.value){
+                imgNode.src=input_file.value;
+                Slago.LoadPage.hover();//进行悬浮层
+            }
+        }
+        //点击表单
+        input.click();
+    };
     namespace.getModel=function(){
         return Slagolib.template.engin(this.Page,{});
-    }
+    };
     namespace.show=function(){
         //创建页面,推入页面栈
         Slago.CreatePage(this.getModel());
-    }
+    };
     //加入模块
     SlagoModel.UserPersonal.UserData.userDataPage=namespace;
 })();
